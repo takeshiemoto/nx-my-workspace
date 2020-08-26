027027c03cc9d314d991348a9db05727bf359797
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-08-27 07:04:16 +0900

    feat: dev

--- apps/pwa-app/next.config.js
+++ apps/pwa-app/next.config.js
@@ -1,8 +1,7 @@
-const withOffline = require('next-offline')
+const withOffline = require('next-offline');
 const nextConfig = {
   target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest),
 };
 
-module.exports = withOffline(nextConfig)
-
-
+module.exports = withOffline(nextConfig);
